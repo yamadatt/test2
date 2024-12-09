19a6eb5b8085ba2c99b19ed478faf73a58f1d3e6
yamadatt <yamadatt@gmail.com>
2024-12-09 09:35:49 +0900

    Add unit tests for main application logic
    Related to #3
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/yamadatt/test2/issues/3?shareId=XXXX-XXXX-XXXX-XXXX).

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ go run main.go <arg1> <arg2>
 ```
 
 Replace `<arg1>` and `<arg2>` with the two numbers you want to add.
+
+## Running Unit Tests
+
+To run the unit tests for the Golang application, use the following command:
+
+```sh
+go test
+```
---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	tests := []struct {
+		name      string
+		args      []string
+		want      string
+		expectErr bool
+	}{
+		{"ValidArgs", []string{"main.go", "2", "3"}, "Sum: 5\n", false},
+		{"InvalidArgs", []string{"main.go", "a", "3"}, "Both arguments must be integers.\n", true},
+		{"MissingArgs", []string{"main.go", "2"}, "Usage: go run main.go <arg1> <arg2>\n", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			oldArgs := os.Args
+			defer func() { os.Args = oldArgs }()
+			os.Args = tt.args
+
+			if got := captureOutput(main); got != tt.want {
+				t.Errorf("main() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func captureOutput(f func()) string {
+	r, w, _ := os.Pipe()
+	stdout := os.Stdout
+	defer func() { os.Stdout = stdout }()
+	os.Stdout = w
+
+	f()
+	w.Close()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
